ce408e532baed1e46480fe966a9b868c99fbdb4e
peusterm <manuel.peuster@uni-paderborn.de>
2018-01-26 15:37:15 +0100

    Added VNF package example
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

---
+++ package-specification/test/test_5gtango-vnf-package-example.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+##
+## Test package examples against schemas,
+## e.g., NAPD, NSDs, VNFDs, REFs, ...
+
+CMD=yamlvalidate
+BASE_DIR=`dirname $0`
+# schema paths
+NAPD_SCHEMA=${BASE_DIR}/../napd-schema.yml
+REF_SCHEMA=${BASE_DIR}/../ref-schema.yml
+NSD_SCHEMA=${BASE_DIR}/../../service-descriptor/nsd-schema.yml
+VNFD_SCHEMA=${BASE_DIR}/../../function-descriptor/vnfd-schema.yml
+
+#
+# Execute the tests.
+#
+${CMD} -s ${NAPD_SCHEMA} -y ${BASE_DIR}/../examples/5gtango-vnf-package-example/TOSCA-Metadata/NAPD.yaml
+${CMD} -s ${VNFD_SCHEMA} -y ${BASE_DIR}/../examples/5gtango-vnf-package-example/Definitions/myvnfd.yaml
+${CMD} -s ${REF_SCHEMA} -y ${BASE_DIR}/../examples/5gtango-vnf-package-example/Images/mycloudimage.ref
