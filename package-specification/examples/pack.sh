dc0fc42c3a813027e9bc26c349e8748ebc060a82
peusterm <manuel.peuster@uni-paderborn.de>
2018-05-02 13:30:49 +0200

    Added script to package examples.
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

---
+++ package-specification/examples/pack.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd 5gtango-ns-package-example; zip -r ../5gtango-ns-package-example.tgo ./; cd ..;
+cd 5gtango-vnf-package-example; zip -r ../5gtango-vnf-package-example.tgo ./; cd ..;
+cd 5gtango-test-package-example; zip -r ../5gtango-test-package-example.tgo ./; cd ..;
